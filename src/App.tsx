9171f57a8ee0e5c765abb10bad985d281f0a2578
Gr8khan12 <gr8khan1629@gmail.com>
2026-08-09 13:07:18 -0700

    Task 9: search + global nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Button from "./componets/Button";
-import Card from "./componets/Card";
+import Button from "./components/Button";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
